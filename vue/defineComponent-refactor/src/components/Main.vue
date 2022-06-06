f811475d9ca74364f4ef073d09a850c550e4b60c
zhangyong <zhangyong@uino.com>
2022-06-06 17:25:28 +0800

    feat: 主容器

---
+++ vue/defineComponent-refactor/src/components/Main.vue
@@ -0,0 +1,12 @@
+<template>
+  <div></div>
+</template>
+<script>
+import { defineComponent } from 'vue';
+export default defineComponent({
+  props: {},
+  setup(props) {
+    return {};
+  }
+});
+</script>
